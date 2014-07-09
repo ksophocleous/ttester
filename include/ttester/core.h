99de4a085d9f24d26abd65baa36d5dd93f4b9558
Konstantinos Sofokleous <ksophocleous@gmail.com>
2014-07-09 14:58:14 +0200

    renamed strigify macro to something more ttester unique

--- include/ttester/core.h
+++ include/ttester/core.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#define stringify(_s) #_s
-#define expect_true(_x) { if ((_x) == false) throw tsuite_exception(strobj() << "expect_true(" << stringify(_x) << ") on " << __FILE__ << ":" << __LINE__); }
+#define ttester_stringify(_s) #_s
+#define expect_true(_x) { if ((_x) == false) throw tsuite_exception(strobj() << "expect_true(" << ttester_stringify(_x) << ") on " << __FILE__ << ":" << __LINE__); }
 
 void breakdebugger();
